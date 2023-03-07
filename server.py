7c76e85b3018c799e623ebff78b3a931d099ee20
Kamal Debnath <76668054+kamaldebnath@users.noreply.github.com>
2023-03-07 18:29:08 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import createimage, metadata, ipfs_storage
+import createimage, metadata, ipfs_storage,cleardata
 from flask import Flask, render_template, request
 
 app = Flask(__name__, static_folder='images')
@@ -10,9 +10,10 @@ def home():
         promt = request.form["promt"]
         if promt:
             img_link = createimage.create(prompt=promt)
-#             ipfs_img = ipfs_storage.create_ipfs(f'./images/{promt[0:4]}.png')
+            # ipfs_img = ipfs_storage.create_ipfs(f'./images/{promt[0:4]}.png')
             ipfs_img= ipfs_storage.pinata_ipfs(f'./images/{promt[0:4]}.png')
             nft_metadata = metadata.createMetadata(ipfs_img, promt)
+            cleardata.remove(promt[0:4])
             return render_template("home.html", nft_metadata=nft_metadata,file_name=img_link)
     else:
         return render_template("home.html")
