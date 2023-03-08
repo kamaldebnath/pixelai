bd7367136cf25dd8a1a0ae2b5708b5beab672d41
Kamal Debnath <76668054+kamaldebnath@users.noreply.github.com>
2023-03-08 18:04:36 +0530

    Update ipfs_storage.py

--- ipfs_storage.py
+++ ipfs_storage.py
@@ -1,8 +1,19 @@
-import pinata,os
+import nft_storage,os
+from nft_storage.api import nft_storage_api
 
-pinata = pinata.Pinata(os.environ.get('PINATA_API'), os.environ.get("PINATA_SECRET_KEY"),os.environ.get("PINATA_ACCESS_TOKEN"))
 
+def create_ipfs(object):
+    configuration = nft_storage.Configuration(
+        access_token=os.environ.get('NFT_STORAGE_API')
 
-def pinata_ipfs(object):
-    file = pinata.pin_file(object)
-    return file['data']['IpfsHash']
+    )
+
+    with nft_storage.ApiClient(configuration) as api_client:
+        api_instance = nft_storage_api.NFTStorageAPI(api_client)
+
+        with open(object, 'rb') as body:
+            try:
+                api_response = api_instance.store(body, _check_return_type=False)
+                return api_response['value']['cid']
+            except nft_storage.ApiException as e:
+                print("Exception when calling NFTStorageAPI->store: %s\n" % e)
